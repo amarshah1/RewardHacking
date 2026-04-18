b2badab8ac8f9adc720764fecd1297bcb6504e3d
Elanor Tang <elanor@umich.edu>
2026-04-18 14:28:03 -0400

    improve 97

--- data/oracle_test_cache/HumanEval_97.rs
+++ data/oracle_test_cache/HumanEval_97.rs
@@ -1,191 +1,191 @@
 #[test]
 fn oracle_case_0() {
-    let arg_0: i32 = 2;
-    let arg_1: i32 = 0;
-    let expected: i32 = 0;
+    let arg_0: i32 = 1212;
+    let arg_1: i32 = 4516;
+    let expected: i32 = 12;
     assert_eq!(multiply(arg_0, arg_1), expected);
 }
 
 #[test]
 fn oracle_case_1() {
-    let arg_0: i32 = 0;
-    let arg_1: i32 = -6;
-    let expected: i32 = 0;
-    assert_eq!(multiply(arg_0, arg_1), expected);
-}
-
-#[test]
-fn oracle_case_2() {
-    let arg_0: i32 = 3;
-    let arg_1: i32 = 0;
-    let expected: i32 = 0;
-    assert_eq!(multiply(arg_0, arg_1), expected);
-}
-
-#[test]
-fn oracle_case_3() {
-    let arg_0: i32 = 3;
-    let arg_1: i32 = 5;
-    let expected: i32 = 15;
-    assert_eq!(multiply(arg_0, arg_1), expected);
-}
-
-#[test]
-fn oracle_case_4() {
-    let arg_0: i32 = 0;
-    let arg_1: i32 = -2;
-    let expected: i32 = 0;
-    assert_eq!(multiply(arg_0, arg_1), expected);
-}
-
-#[test]
-fn oracle_case_5() {
-    let arg_0: i32 = 0;
-    let arg_1: i32 = 6;
-    let expected: i32 = 0;
-    assert_eq!(multiply(arg_0, arg_1), expected);
-}
-
-#[test]
-fn oracle_case_6() {
-    let arg_0: i32 = -7;
-    let arg_1: i32 = -2;
-    let expected: i32 = 14;
-    assert_eq!(multiply(arg_0, arg_1), expected);
-}
-
-#[test]
-fn oracle_case_7() {
-    let arg_0: i32 = 4;
-    let arg_1: i32 = 0;
-    let expected: i32 = 0;
-    assert_eq!(multiply(arg_0, arg_1), expected);
-}
-
-#[test]
-fn oracle_case_8() {
-    let arg_0: i32 = 11;
-    let arg_1: i32 = -2;
-    let expected: i32 = 2;
-    assert_eq!(multiply(arg_0, arg_1), expected);
-}
-
-#[test]
-fn oracle_case_9() {
-    let arg_0: i32 = 0;
-    let arg_1: i32 = -3;
-    let expected: i32 = 0;
-    assert_eq!(multiply(arg_0, arg_1), expected);
-}
-
-#[test]
-fn oracle_case_10() {
-    let arg_0: i32 = -4;
-    let arg_1: i32 = 2;
+    let arg_0: i32 = -4004;
+    let arg_1: i32 = -3492;
     let expected: i32 = 8;
     assert_eq!(multiply(arg_0, arg_1), expected);
 }
 
 #[test]
+fn oracle_case_2() {
+    let arg_0: i32 = -4202;
+    let arg_1: i32 = 3747;
+    let expected: i32 = 14;
+    assert_eq!(multiply(arg_0, arg_1), expected);
+}
+
+#[test]
+fn oracle_case_3() {
+    let arg_0: i32 = 4011;
+    let arg_1: i32 = 476;
+    let expected: i32 = 6;
+    assert_eq!(multiply(arg_0, arg_1), expected);
+}
+
+#[test]
+fn oracle_case_4() {
+    let arg_0: i32 = -3119;
+    let arg_1: i32 = 446;
+    let expected: i32 = 54;
+    assert_eq!(multiply(arg_0, arg_1), expected);
+}
+
+#[test]
+fn oracle_case_5() {
+    let arg_0: i32 = 1551;
+    let arg_1: i32 = 111;
+    let expected: i32 = 1;
+    assert_eq!(multiply(arg_0, arg_1), expected);
+}
+
+#[test]
+fn oracle_case_6() {
+    let arg_0: i32 = 2262;
+    let arg_1: i32 = 3875;
+    let expected: i32 = 10;
+    assert_eq!(multiply(arg_0, arg_1), expected);
+}
+
+#[test]
+fn oracle_case_7() {
+    let arg_0: i32 = -2007;
+    let arg_1: i32 = -1063;
+    let expected: i32 = 21;
+    assert_eq!(multiply(arg_0, arg_1), expected);
+}
+
+#[test]
+fn oracle_case_8() {
+    let arg_0: i32 = -3306;
+    let arg_1: i32 = 3752;
+    let expected: i32 = 12;
+    assert_eq!(multiply(arg_0, arg_1), expected);
+}
+
+#[test]
+fn oracle_case_9() {
+    let arg_0: i32 = -4255;
+    let arg_1: i32 = -3776;
+    let expected: i32 = 30;
+    assert_eq!(multiply(arg_0, arg_1), expected);
+}
+
+#[test]
+fn oracle_case_10() {
+    let arg_0: i32 = 4032;
+    let arg_1: i32 = 3718;
+    let expected: i32 = 16;
+    assert_eq!(multiply(arg_0, arg_1), expected);
+}
+
+#[test]
 fn oracle_case_11() {
-    let arg_0: i32 = 2;
-    let arg_1: i32 = 1;
-    let expected: i32 = 2;
+    let arg_0: i32 = -623;
+    let arg_1: i32 = 3952;
+    let expected: i32 = 6;
     assert_eq!(multiply(arg_0, arg_1), expected);
 }
 
 #[test]
 fn oracle_case_12() {
-    let arg_0: i32 = 1;
-    let arg_1: i32 = -5;
-    let expected: i32 = 5;
+    let arg_0: i32 = 4899;
+    let arg_1: i32 = -4881;
+    let expected: i32 = 9;
     assert_eq!(multiply(arg_0, arg_1), expected);
 }
 
 #[test]
 fn oracle_case_13() {
-    let arg_0: i32 = -1;
-    let arg_1: i32 = 2;
-    let expected: i32 = 2;
+    let arg_0: i32 = -4629;
+    let arg_1: i32 = -3851;
+    let expected: i32 = 9;
     assert_eq!(multiply(arg_0, arg_1), expected);
 }
 
 #[test]
 fn oracle_case_14() {
-    let arg_0: i32 = -1;
-    let arg_1: i32 = -2;
-    let expected: i32 = 2;
+    let arg_0: i32 = -3317;
+    let arg_1: i32 = -4174;
+    let expected: i32 = 28;
     assert_eq!(multiply(arg_0, arg_1), expected);
 }
 
 #[test]
 fn oracle_case_15() {
-    let arg_0: i32 = -2;
-    let arg_1: i32 = 1;
+    let arg_0: i32 = -3132;
+    let arg_1: i32 = 1491;
     let expected: i32 = 2;
     assert_eq!(multiply(arg_0, arg_1), expected);
 }
 
 #[test]
 fn oracle_case_16() {
-    let arg_0: i32 = -2;
-    let arg_1: i32 = -1;
-    let expected: i32 = 2;
+    let arg_0: i32 = 2508;
+    let arg_1: i32 = -2143;
+    let expected: i32 = 24;
     assert_eq!(multiply(arg_0, arg_1), expected);
 }
 
 #[test]
 fn oracle_case_17() {
-    let arg_0: i32 = 4;
-    let arg_1: i32 = -1;
-    let expected: i32 = 4;
+    let arg_0: i32 = 2001;
+    let arg_1: i32 = -1380;
+    let expected: i32 = 0;
     assert_eq!(multiply(arg_0, arg_1), expected);
 }
 
 #[test]
 fn oracle_case_18() {
-    let arg_0: i32 = -4;
-    let arg_1: i32 = 4;
-    let expected: i32 = 16;
+    let arg_0: i32 = 2044;
+    let arg_1: i32 = -4551;
+    let expected: i32 = 4;
     assert_eq!(multiply(arg_0, arg_1), expected);
 }
 
 #[test]
 fn oracle_case_19() {
-    let arg_0: i32 = 3;
-    let arg_1: i32 = -2;
-    let expected: i32 = 6;
+    let arg_0: i32 = -416;
+    let arg_1: i32 = 1589;
+    let expected: i32 = 54;
     assert_eq!(multiply(arg_0, arg_1), expected);
 }
 
 #[test]
 fn oracle_case_20() {
-    let arg_0: i32 = -2;
-    let arg_1: i32 = 0;
-    let expected: i32 = 0;
+    let arg_0: i32 = 435;
+    let arg_1: i32 = -3881;
+    let expected: i32 = 5;
     assert_eq!(multiply(arg_0, arg_1), expected);
 }
 
 #[test]
 fn oracle_case_21() {
-    let arg_0: i32 = 5;
-    let arg_1: i32 = -3;
-    let expected: i32 = 15;
+    let arg_0: i32 = -4900;
+    let arg_1: i32 = 176;
+    let expected: i32 = 0;
     assert_eq!(multiply(arg_0, arg_1), expected);
 }
 
 #[test]
 fn oracle_case_22() {
-    let arg_0: i32 = 14;
-    let arg_1: i32 = 4;
-    let expected: i32 = 16;
+    let arg_0: i32 = -4043;
+    let arg_1: i32 = -1667;
+    let expected: i32 = 21;
     assert_eq!(multiply(arg_0, arg_1), expected);
 }
 
 #[test]
 fn oracle_case_23() {
-    let arg_0: i32 = 0;
-    let arg_1: i32 = 0;
-    let expected: i32 = 0;
+    let arg_0: i32 = -1758;
+    let arg_1: i32 = 2153;
+    let expected: i32 = 24;
     assert_eq!(multiply(arg_0, arg_1), expected);
 }
