1dfb836f15b825b60e995fbcbf24987e2d65323e
Elanor Tang <elanor@umich.edu>
2026-04-18 14:55:24 -0400

    look at 25

--- data/oracle_test_cache/HumanEval_25.rs
+++ data/oracle_test_cache/HumanEval_25.rs
@@ -1,167 +1,167 @@
 #[test]
 fn oracle_case_0() {
-    let arg_0: u8 = 57;
-    let expected: Vec<u8> = vec![3, 19];
+    let arg_0: u8 = 4;
+    let expected: Vec<u8> = vec![2, 2];
     assert_eq!(factorize(arg_0), expected);
 }
 
 #[test]
 fn oracle_case_1() {
-    let arg_0: u8 = 217;
-    let expected: Vec<u8> = vec![7, 31];
+    let arg_0: u8 = 239;
+    let expected: Vec<u8> = vec![239];
     assert_eq!(factorize(arg_0), expected);
 }
 
 #[test]
 fn oracle_case_2() {
-    let arg_0: u8 = 141;
-    let expected: Vec<u8> = vec![3, 47];
+    let arg_0: u8 = 219;
+    let expected: Vec<u8> = vec![3, 73];
     assert_eq!(factorize(arg_0), expected);
 }
 
 #[test]
 fn oracle_case_3() {
+    let arg_0: u8 = 196;
+    let expected: Vec<u8> = vec![2, 2, 7, 7];
+    assert_eq!(factorize(arg_0), expected);
+}
+
+#[test]
+fn oracle_case_4() {
+    let arg_0: u8 = 213;
+    let expected: Vec<u8> = vec![3, 71];
+    assert_eq!(factorize(arg_0), expected);
+}
+
+#[test]
+fn oracle_case_5() {
+    let arg_0: u8 = 142;
+    let expected: Vec<u8> = vec![2, 71];
+    assert_eq!(factorize(arg_0), expected);
+}
+
+#[test]
+fn oracle_case_6() {
+    let arg_0: u8 = 67;
+    let expected: Vec<u8> = vec![67];
+    assert_eq!(factorize(arg_0), expected);
+}
+
+#[test]
+fn oracle_case_7() {
+    let arg_0: u8 = 222;
+    let expected: Vec<u8> = vec![2, 3, 37];
+    assert_eq!(factorize(arg_0), expected);
+}
+
+#[test]
+fn oracle_case_8() {
+    let arg_0: u8 = 82;
+    let expected: Vec<u8> = vec![2, 41];
+    assert_eq!(factorize(arg_0), expected);
+}
+
+#[test]
+fn oracle_case_9() {
+    let arg_0: u8 = 155;
+    let expected: Vec<u8> = vec![5, 31];
+    assert_eq!(factorize(arg_0), expected);
+}
+
+#[test]
+fn oracle_case_10() {
+    let arg_0: u8 = 220;
+    let expected: Vec<u8> = vec![2, 2, 5, 11];
+    assert_eq!(factorize(arg_0), expected);
+}
+
+#[test]
+fn oracle_case_11() {
+    let arg_0: u8 = 39;
+    let expected: Vec<u8> = vec![3, 13];
+    assert_eq!(factorize(arg_0), expected);
+}
+
+#[test]
+fn oracle_case_12() {
+    let arg_0: u8 = 75;
+    let expected: Vec<u8> = vec![3, 5, 5];
+    assert_eq!(factorize(arg_0), expected);
+}
+
+#[test]
+fn oracle_case_13() {
+    let arg_0: u8 = 246;
+    let expected: Vec<u8> = vec![2, 3, 41];
+    assert_eq!(factorize(arg_0), expected);
+}
+
+#[test]
+fn oracle_case_14() {
+    let arg_0: u8 = 240;
+    let expected: Vec<u8> = vec![2, 2, 2, 2, 3, 5];
+    assert_eq!(factorize(arg_0), expected);
+}
+
+#[test]
+fn oracle_case_15() {
+    let arg_0: u8 = 154;
+    let expected: Vec<u8> = vec![2, 7, 11];
+    assert_eq!(factorize(arg_0), expected);
+}
+
+#[test]
+fn oracle_case_16() {
+    let arg_0: u8 = 244;
+    let expected: Vec<u8> = vec![2, 2, 61];
+    assert_eq!(factorize(arg_0), expected);
+}
+
+#[test]
+fn oracle_case_17() {
+    let arg_0: u8 = 226;
+    let expected: Vec<u8> = vec![2, 113];
+    assert_eq!(factorize(arg_0), expected);
+}
+
+#[test]
+fn oracle_case_18() {
     let arg_0: u8 = 184;
     let expected: Vec<u8> = vec![2, 2, 2, 23];
     assert_eq!(factorize(arg_0), expected);
 }
 
 #[test]
-fn oracle_case_4() {
+fn oracle_case_19() {
+    let arg_0: u8 = 97;
+    let expected: Vec<u8> = vec![97];
+    assert_eq!(factorize(arg_0), expected);
+}
+
+#[test]
+fn oracle_case_20() {
+    let arg_0: u8 = 106;
+    let expected: Vec<u8> = vec![2, 53];
+    assert_eq!(factorize(arg_0), expected);
+}
+
+#[test]
+fn oracle_case_21() {
+    let arg_0: u8 = 146;
+    let expected: Vec<u8> = vec![2, 73];
+    assert_eq!(factorize(arg_0), expected);
+}
+
+#[test]
+fn oracle_case_22() {
     let arg_0: u8 = 138;
     let expected: Vec<u8> = vec![2, 3, 23];
     assert_eq!(factorize(arg_0), expected);
 }
 
 #[test]
-fn oracle_case_5() {
-    let arg_0: u8 = 188;
-    let expected: Vec<u8> = vec![2, 2, 47];
-    assert_eq!(factorize(arg_0), expected);
-}
-
-#[test]
-fn oracle_case_6() {
-    let arg_0: u8 = 178;
-    let expected: Vec<u8> = vec![2, 89];
-    assert_eq!(factorize(arg_0), expected);
-}
-
-#[test]
-fn oracle_case_7() {
-    let arg_0: u8 = 216;
-    let expected: Vec<u8> = vec![2, 2, 2, 3, 3, 3];
-    assert_eq!(factorize(arg_0), expected);
-}
-
-#[test]
-fn oracle_case_8() {
-    let arg_0: u8 = 43;
-    let expected: Vec<u8> = vec![43];
-    assert_eq!(factorize(arg_0), expected);
-}
-
-#[test]
-fn oracle_case_9() {
-    let arg_0: u8 = 162;
-    let expected: Vec<u8> = vec![2, 3, 3, 3, 3];
-    assert_eq!(factorize(arg_0), expected);
-}
-
-#[test]
-fn oracle_case_10() {
-    let arg_0: u8 = 63;
-    let expected: Vec<u8> = vec![3, 3, 7];
-    assert_eq!(factorize(arg_0), expected);
-}
-
-#[test]
-fn oracle_case_11() {
-    let arg_0: u8 = 58;
-    let expected: Vec<u8> = vec![2, 29];
-    assert_eq!(factorize(arg_0), expected);
-}
-
-#[test]
-fn oracle_case_12() {
-    let arg_0: u8 = 37;
-    let expected: Vec<u8> = vec![37];
-    assert_eq!(factorize(arg_0), expected);
-}
-
-#[test]
-fn oracle_case_13() {
-    let arg_0: u8 = 177;
-    let expected: Vec<u8> = vec![3, 59];
-    assert_eq!(factorize(arg_0), expected);
-}
-
-#[test]
-fn oracle_case_14() {
-    let arg_0: u8 = 194;
-    let expected: Vec<u8> = vec![2, 97];
-    assert_eq!(factorize(arg_0), expected);
-}
-
-#[test]
-fn oracle_case_15() {
-    let arg_0: u8 = 72;
-    let expected: Vec<u8> = vec![2, 2, 2, 3, 3];
-    assert_eq!(factorize(arg_0), expected);
-}
-
-#[test]
-fn oracle_case_16() {
-    let arg_0: u8 = 24;
-    let expected: Vec<u8> = vec![2, 2, 2, 3];
-    assert_eq!(factorize(arg_0), expected);
-}
-
-#[test]
-fn oracle_case_17() {
-    let arg_0: u8 = 173;
-    let expected: Vec<u8> = vec![173];
-    assert_eq!(factorize(arg_0), expected);
-}
-
-#[test]
-fn oracle_case_18() {
-    let arg_0: u8 = 19;
-    let expected: Vec<u8> = vec![19];
-    assert_eq!(factorize(arg_0), expected);
-}
-
-#[test]
-fn oracle_case_19() {
-    let arg_0: u8 = 175;
-    let expected: Vec<u8> = vec![5, 5, 7];
-    assert_eq!(factorize(arg_0), expected);
-}
-
-#[test]
-fn oracle_case_20() {
-    let arg_0: u8 = 248;
-    let expected: Vec<u8> = vec![2, 2, 2, 31];
-    assert_eq!(factorize(arg_0), expected);
-}
-
-#[test]
-fn oracle_case_21() {
-    let arg_0: u8 = 225;
-    let expected: Vec<u8> = vec![3, 3, 5, 5];
-    assert_eq!(factorize(arg_0), expected);
-}
-
-#[test]
-fn oracle_case_22() {
-    let arg_0: u8 = 236;
-    let expected: Vec<u8> = vec![2, 2, 59];
-    assert_eq!(factorize(arg_0), expected);
-}
-
-#[test]
 fn oracle_case_23() {
-    let arg_0: u8 = 42;
-    let expected: Vec<u8> = vec![2, 3, 7];
+    let arg_0: u8 = 84;
+    let expected: Vec<u8> = vec![2, 2, 3, 7];
     assert_eq!(factorize(arg_0), expected);
 }
